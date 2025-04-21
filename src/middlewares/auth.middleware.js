ad99415a34160e0f26f1929a59681a2901fc07a2
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2025-04-21 22:09:16 +0530

    -

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,20 +4,24 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/users.models.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-
-    if (!token) {
-        throw new apiError(401, "No token provided, authorization denied");
+    try {
+        const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    
+        if (!token) {
+            throw new apiError(401, "No token provided, authorization denied");
+        }
+        //NOTE: 
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+        const user = User.findByIdAndDelete(decodedToken._id).select("--password --refreshToken");
+    
+        if (!user) {
+            throw new apiError(401, "Token is not valid");
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new apiError(401, error?.message || "Token is not valid");
     }
-    //NOTE: 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    const user = User.findByIdAndDelete(decodedToken._id).select("--password --refreshToken");
-
-    if (!user) {
-        throw new apiError(401, "Token is not valid");
-    }
-
-    req.user = user;
-    next();
 })
