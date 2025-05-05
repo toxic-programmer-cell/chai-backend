9aaa3126704e2a73fcb14f1590941fa21e30bb49
rohit <rohitmahto062@gmail.com>
2025-05-05 23:34:40 +0530

    feat: define Playlist and Tweet models with mongoose schemas

--- src/models/playlist.models.js
+++ src/models/playlist.models.js
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
 
 const playlistSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 },{timestamps: true})
 
 
